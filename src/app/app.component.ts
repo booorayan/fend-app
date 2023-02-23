55748e79a20a60009d919cf4c13afa39a393f97c
brian <bryantlch@gmail.com>
2023-02-23 15:30:55 +0300

    data binding and ng directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h1>Frontend Dev</h1>',
   styleUrls: ['./app.component.css']
+  // styles: ['h1{color: darkcyan}']
 })
 export class AppComponent {
-  title = 'fend-app';
+  title = 'App Development with Angular';
+  nums = [2030,3040,23,46,98,87,109];
+
+  sum = ()=>{
+    return this.nums[0] + this.nums[1];
+  }
+
+  darkcyan = "color: darkcyan";
+  dcyan = "background-color: darkcyan";
+  green = "background-color: darkseagreen";
+  cadetblue = "color: cadetblue";
+  // cblue = "background-color: cadetblue";
+
+  message = "";
+  clickMe = (val: any) =>{
+    console.log("Clicked !! !!");
+    console.log(val);
+    this.message = val;
+  }
+
+  show=true;
+  toggle() {
+    this.show = !this.show;
+  }
+
+  inc(){
+    this.nums.push( Math.floor(Math.random() * 100));
+  }
+
+  dec(){
+    this.nums.pop();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AboutComponent } from './about/about.component';
+import {AboutModule} from "./about/about.module";
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // LoginComponent,
+    // RegisterComponent,
+    // AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AboutModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
